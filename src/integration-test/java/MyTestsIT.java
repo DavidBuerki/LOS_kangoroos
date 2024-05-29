7f15e6fb628ecaf483c714902e066fa66d49cbbd
goncalomartinscorreia <goncalo.martins@he-arc.ch>
2024-05-29 11:02:54 +0200

    Exercice4_Part1_Part2

---
+++ src/integration-test/java/MyTestsIT.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MyTestsIT {
+    @Test
+    public void simpleTest_Given1_ShouldBe1() {
+        assertEquals(1, 1);
+    }
+
+
+}
